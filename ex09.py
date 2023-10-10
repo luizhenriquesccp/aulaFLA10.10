7a9c37c564725e6d9c9b1b3e5eb5ef26e651e1f3
luizhenriquesccp <147549836+luizhenriquesccp@users.noreply.github.com>
2023-10-10 17:55:19 -0300

    Add files via upload

--- ex09.py
+++ ex09.py
@@ -1,10 +1,32 @@
 import turtle
+rick = turtle.Turtle()
 
-leo = turtle.Turtle()
-
-def forma ():
-	for _ in range(6):
-		leo.forward(25)
-		leo.right(60)
-		
+def desenhe_hexagono():
+    for _ in range(4):
+        rick.forward(25)
+        rick.right(90)
+       
+for _ in range(6):
+    for _ in range(2):
+        for _ in range(4):
+            desenhe_hexagono()
+            rick.penup()
+            rick.forward(75)
+            rick.pendown()
+        rick.penup()
+        rick.backward(25)
+        rick.pendown()
+        rick.right(60)
+        rick.penup()
+        rick.forward(25)
+        rick.pendown()
+        rick.right(120)
+    rick.penup()
+    rick.backward(25)
+    rick.pendown()
+    rick.left(60)
+    rick.penup()
+    rick.forward(50)
+    rick.pendown()
+    rick.right(60)
 
